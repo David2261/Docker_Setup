579ed7fa6891ad28b94c5d1961a9f875e1c145a0
David2261 <bulatnasirov2003@gmail.com>
2022-08-22 00:47:02 +0300

    Update directory

--- src/views/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div>
-		<h2>Home page</h2>
-		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis perferendis rerum fuga, illum! Autem suscipit provident quos ullam consequatur neque rerum, sit deserunt, porro, corporis officiis vel dolorem? Molestias, harum.</p>
-		<a href="/todos">Todos</a>
-		<router-view to="/todos" />
-	</div>
-</template>
