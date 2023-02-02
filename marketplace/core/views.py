15dffbdd53f1e4e2d12ba7d003d493bb874875d1
David2261 <bulatnasirov2003@gmail.com>
2023-02-02 19:05:54 +0300

    Add signup for marketplace

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
 from item.models import Category, Item
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
+
 
 def index(request):
 	items = Item.objects.filter(is_sold=False)[0:6]
