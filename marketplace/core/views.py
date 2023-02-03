533677446b1f38810d928333af62f1df2ff728b9
David2261 <bulatnasirov2003@gmail.com>
2023-02-03 23:58:57 +0300

    Lint with flake8

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from item.models import Category, Item
 from .forms import SignupForm
 
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
 
-        if form.is_valid():
-            form.save()
+		if form.is_valid():
+			form.save()
 
-            return redirect('/login/')
-    else:
-        form = SignupForm()
+			return redirect('/login/')
+	else:
+		form = SignupForm()
 
-    return render(request, 'core/signup.html', {
-        'form': form
-    })
+	return render(request, 'core/signup.html', {
+		'form': form
+	})
 
 
 def index(request):
 	items = Item.objects.filter(is_sold=False)[0:6]
 	categories = Category.objects.all()
 
-	return render(request, 'core/index.html', {'categories': categories, 'items': items})
+	return render(request, 'core/index.html', {
+		'categories': categories,
+		'items': items
+	})
 
 
 def contact(request):
